a267781bedd89436578ff1803d089fe42f87912a
mmakau0924 <mmakau0924@starkstate.net>
2023-03-03 18:23:54 +0300

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LB6_CoreWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+       
+        public string? Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = $"Hellow World {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
